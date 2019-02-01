8ff59107e44bd1bc3c3c3d874346e790663ca05d
Lorenzo <lorenzobugay@gmail.com>
2019-02-01 22:49:23 +0800

    New changes

--- app/src/main/java/com/example/bantay/bantay/MapFragment.java
+++ app/src/main/java/com/example/bantay/bantay/MapFragment.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -17,6 +19,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -34,6 +37,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -41,6 +48,8 @@ import com.google.android.gms.tasks.Task;
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     //vars
+
+
     GoogleMap map;
     FusedLocationProviderClient fusedLocationProviderClient;
     float DEFAULT_ZOOM = 17f;
@@ -48,6 +57,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public final static int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     //Bounds compass(sw, ne)
     public LatLngBounds MARIKINA = new LatLngBounds(new LatLng(14.618055, 121.079299), new LatLng(14.673361, 121.131398));
+    public TextView mapAddress;
+    Geocoder geocoder;
+    List<Address> addressList;
     //Evacuation center markers
     public Marker SampaguitaGym;
 
@@ -124,16 +136,14 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         //Current location of user
         if (mLocationPermissionsGranted) {
             getDeviceLocation();
-            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 getLocationPermission();
             }
             map.setMyLocationEnabled(true);
             map.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
                 @Override
                 public boolean onMyLocationButtonClick() {
-                    if (true){
-                        getDeviceLocation();
-                    }
+                    getDeviceLocation();
                     return false;
                 }
             });
@@ -167,6 +177,24 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                         if(task.isSuccessful() && task.getResult() != null){
                             Location currentLocation = (Location) task.getResult();
                             moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+
+                            geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                            try {
+                                addressList = geocoder.getFromLocation(currentLocation.getLatitude(), currentLocation.getLongitude(), 1);
+                              //  String address = addressList.get(0).getAddressLine(0);
+
+                                String addressline = addressList.get(0).getAddressLine(0);
+                                String thoroughfare = addressList.get(0).getThoroughfare(); //Street
+                                String locality = addressList.get(0).getLocality(); //City or Marikina
+                                String adminarea = addressList.get(0).getAdminArea(); //Capital or Metro Manila
+                               // String fulladdress = thoroughfare+", "+locality+", "+adminarea;
+                                String fulladdress = addressline;
+                                mapAddress = getView().findViewById(R.id.mapAddress);
+                                mapAddress.setText(fulladdress);
+                            }
+                            catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
                         else{
                             moveCamera(new LatLng(14.647329, 121.104834), 10f);
@@ -183,4 +211,3 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
 
 }
-
